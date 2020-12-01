a14d0f90d7e07717069aa2990addc509a498372b
zhucy9974 <75172940+zhucy9974@users.noreply.github.com>
2020-12-01 15:57:34 +0100

    déplacer le composant de recherche
    déplacer le composant de recherche

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,15 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users.component';
 import {SharedModule} from "../shared/shared.module";
-import {NavbarModule} from "../navbar/navbar.module"
-import { SearchComponent } from '../navbar/search/search.component';
-import { SearchModule } from '../navbar/search/search.module';
+import { SearchModule } from './search/search.module';
 import { FormsModule } from '@angular/forms';
-import { UserPageComponent } from './user-page/user-page.component';
-import { InputComponent } from '../shared/form/input/input.component';
 import { UserPageModule } from './user-page/user-page.module';
-
-
 
 @NgModule({
     declarations: [UsersComponent],
